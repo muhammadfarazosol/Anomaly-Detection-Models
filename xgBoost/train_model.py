f97974cfa51fe1701c0d93ef0b203380380b446b
muhammadfarazosol <muhammad.faraz@osoltech.com>
2025-07-09 15:51:19 +0500

    Unused files

--- xgBoost/train_model.py
+++
@@ -1,9 +0,0 @@
-# train_model.py
-
-from xgboost import XGBClassifier
-from config import XGBOOST_PARAMS
-
-def train_xgboost(X_train, y_train):
-    model = XGBClassifier(**XGBOOST_PARAMS)
-    model.fit(X_train, y_train)
-    return model
